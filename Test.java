5d9f5a297f9570e90ce6c1b952131fe9142047c4
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2025-10-01 09:53:19 +0700

    Ikan

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("ikan ikan apa yang lucu?");
+        System.out.println("ikan nila");
     }
 }
